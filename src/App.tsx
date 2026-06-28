976154ee67b399e9d8cc7d3f13025d0f4d91e8e7
Mael Eric Marcel Brodkorb <udvuo@student.kit.edu>
2026-06-28 22:56:20 +0200

    Playing audio implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,56 @@
 
 import "./App.css";
 import {invoke} from "@tauri-apps/api/core";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [files, setFiles] = useState<string[]>([])
 
+  useEffect(() => {
+    void loadFiles();
+  }, []);
+
+  async function loadFiles() {
+    try {
+      const result = await invoke<string[]>("list_files", {
+        folder: "C:\\Users\\maelb\\Desktop\\music",
+      });
+      setFiles(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   async function callRust() {
-    await invoke<string>("play_sound");
+    try {
+      if (isPlaying) {
+        await invoke<string>("stop_sound");
+        setIsPlaying(false);
+      } else {
+        await invoke<string>("play_sound");
+        setIsPlaying(true);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   }
+
+
 
   return (
     <main className="container">
 
       <button
           onClick={callRust}
-      />
-
+      >
+        {isPlaying ? "Pause" : "Play"}
+      </button>
+      <ul>
+        {files.map((file) => (
+            <li key={file}>{file}</li>
+        ))}
+      </ul>
     </main>
   );
 }
